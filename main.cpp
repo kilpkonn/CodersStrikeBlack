2665d32101b8bc8136571ebea2b5602f7b3bebb7
taannu <tavo.annus@gmail.com>
2020-04-02 15:50:37 +0300

    Start implementing track memorisation, got silver

--- main.cpp
+++ main.cpp
@@ -4,25 +4,59 @@
 #include <algorithm>
 #include <cmath>
 
+#define CHECKPOINT_RADIUS 600
+#define MAX_THRUST 100
+
 using namespace std;
 
-/**
- * This code automatically collects game data in an infinite loop.
- * It uses the standard input to place data into the game variables such as x and y.
- * YOU DO NOT NEED TO MODIFY THE INITIALIZATION OF THE GAME VARIABLES.
- **/
+struct Checkpoint {
+    int x, y;
+
+    bool operator == (const Checkpoint& c) const {
+        return x == c.x && y == c.y;
+    }
+
+    bool operator != (const Checkpoint& c) const {
+        return x != c.x || y != c.y;
+    }
+};
+
+class Track {
+
+    void onNewCheckpoint(Checkpoint point) {
+        if (allCheckpointsFound) return;
+
+        if (checkpoints.empty()) {
+            checkpoints.push_back(point);
+        } else if (checkpoints.back() != point) {
+            checkpoints.push_back(point);
+            if (checkpoints.front() == point) {
+                allCheckpointsFound = true;
+            }
+        }
+    }
+
+private:
+    vector<Checkpoint> checkpoints;
+    bool allCheckpointsFound = false;
+};
 
 bool canRam(int x, int y, int checkX, int checkY, int opponentX, int opponentY, int angle) {
     bool opponentInFront = abs(opponentX - checkX) < abs(x - checkX) && abs(opponentY < checkY) < abs(y - checkY);
     cerr << " In front:"<< opponentInFront;
     bool opponentClose = abs(opponentY - y) < 600 && abs(opponentX - x) < 600;
     cerr << " close: "<< opponentClose;
-    bool opponentCloseToCheck = abs(opponentY - checkY) < 600 && abs(opponentX - checkX) < 600;
+    bool opponentCloseToCheck = abs(opponentY - checkY) < CHECKPOINT_RADIUS && abs(opponentX - checkX) < CHECKPOINT_RADIUS;
     cerr << " close to check: " << opponentCloseToCheck;
     cerr << " angle: " << angle << endl;
     return opponentInFront && opponentClose && opponentCloseToCheck && abs(angle) < 30;
 }
 
+/**
+ * This code automatically collects game data in an infinite loop.
+ * It uses the standard input to place data into the game variables such as x and y.
+ * YOU DO NOT NEED TO MODIFY THE INITIALIZATION OF THE GAME VARIABLES.
+ **/
 int main() {
     bool boostUsed = false;
     // game loop
@@ -40,8 +74,7 @@ int main() {
         cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle;
         cin >> opponentX >> opponentY;
 
-        int thrust = 100;
-        // cerr << "Abs angle: " << abs(nextCheckpointAngle) << endl;
+        int thrust = MAX_THRUST;
         if ((nextCheckpointDist < 3000 && abs(nextCheckpointAngle) > 60) || abs(nextCheckpointAngle) > 100) {
             thrust = 0;
         }
@@ -49,7 +82,7 @@ int main() {
         cerr << " dist: " << nextCheckpointDist;
         bool ram = canRam(x, y, nextCheckpointX, nextCheckpointY, opponentX, opponentY, nextCheckpointAngle);
         cerr << " can ram: " << canRam;
-        if (nextCheckpointDist < 1000 && !ram) {
+        if (nextCheckpointDist < CHECKPOINT_RADIUS && !ram) {
             thrust = 5;
         }
 
